1167c95e3d49bc0190544d50c5bc21f4f2ad685c
saurav2912 <saurav.saurav08@gmail.com>
2026-03-23 21:02:24 +0530

    First commit

---
+++ src/main/java/com/saurav/azure/WebappMI/WebappMiApplication.java
@@ -0,0 +1,13 @@
+package com.saurav.azure.WebappMI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappMiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappMiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saurav/azure/WebappMI/WebappMiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurav.azure.WebappMI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappMiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
