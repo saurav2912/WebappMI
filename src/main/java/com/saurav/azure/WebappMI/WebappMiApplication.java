606a5a8d53710ffe8774b85b8d0573d3e0e99e94
saurav2912 <saurav.saurav08@gmail.com>
2026-04-08 07:35:04 +0530

    commit

--- src/main/java/com/saurav/azure/WebappMI/WebappMiApplication.java
+++ src/main/java/com/saurav/azure/WebappMI/WebappMiApplication.java
@@ -4,10 +4,15 @@ import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class WebappMiApplication {
 
 	public static void main(String[] args) {
@@ -16,5 +21,16 @@ public class WebappMiApplication {
 		SpringApplication.run(WebappMiApplication.class, args);
 	}
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+
+        return http.build();
+    }
 
 }
